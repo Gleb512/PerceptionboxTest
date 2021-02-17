f2e11ec94028eedbaf82a53e579e24394672ebf7
Gleb512 <glebordinskijj@gmail.com>
2021-02-17 22:17:23 +0200

    Second commit

--- src/utils/objectHelpers.js
+++
@@ -1,8 +0,0 @@
-export const updateObjectIArray = (items, itemId, objectPropName) => {
-    return items.map(u => {
-        if(u[objectPropName] === itemId){
-            return {...u, liked: true}
-        }
-        return u;
-    })
-}
